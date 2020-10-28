9179ccb53e9175b6ec8e52135b7b1acc8a39ac7c
brookwormiest <73608584+brookwormiest@users.noreply.github.com>
2020-10-28 12:05:08 -0700

    Update Dynamic_energy_conservation.py

--- Dynamic_energy_conservation.py
+++ Dynamic_energy_conservation.py
@@ -1,7 +1,18 @@
 
 def main():
 
-    print("Requesting pull... trying this...")
+	class Sensor():
+		"""Represent a sensor."""
+		def __init__(self, name):
+			"""Initialize sensor object."""
+			self.name = name
+		def pullingdata(self):
+			"""Simulate pulling data."""
+			print(self.name + "is pulling data.")
+	my_Sensor = Sensor('Sensor1')
 
+	print(my_Sensor.name + "has pulled data.")
+	my_Sensor.pullingdata()
+		
 if __name__ == "__main__":
     main()
